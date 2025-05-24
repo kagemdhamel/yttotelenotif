bf6deed9911f939fd60544560c49e3bf399c672c
Kagem Dhamel <92947791+kagemdhamel@users.noreply.github.com>
2025-05-25 06:42:18 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'telegram_token' => 'API Bot Telegram', // ganti dengan API Bot dari @Botfather
+    'telegram_chat_id' => '-10XXXXXXXXX', // ganti dengan chat id channel
+    'youtube_api_key' => 'YOUTUBE API V3',
+    'cache_file' => __DIR__ . '/cache.json',
+];
